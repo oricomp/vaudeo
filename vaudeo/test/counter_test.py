7969174b2609d73eb15e80a02119e5514b7f6802
Jacob Long <jacoblong853@gmail.com>
2025-01-26 09:22:12 -0500

    [Vaudeo][Counter] implement actual tests

--- vaudeo/test/counter_test.py
+++ vaudeo/test/counter_test.py
@@ -3,13 +3,85 @@ from cocotb.triggers import FallingEdge, Timer
 from cocotb.clock import Clock
 
 
-@cocotb.test()
-async def counter_test(dut):
+START = 0
+STOP = 10
+STEP = 1
+
+
+async def reset(dut):
+    """Resets the device and enables it"""
+    dut.reset.value = 1
+    await Timer(2, units="ns")
     dut.enable.value = 1
     dut.reset.value = 0
+
+
+def assert_defaults(dut):
+    """Asserts that the default parameters are the same"""
+    assert dut.START.value == START
+    assert dut.STOP.value == STOP
+    assert dut.STEP.value == STEP
+
+
+@cocotb.test()
+async def counter_counts_test(dut):
+    """Test that the counter properly counts"""
+    assert_defaults(dut)
     await cocotb.start(Clock(dut.clk, 1, units="ns").start())
+    await reset(dut)
 
+    assert (STOP - START) // STEP >= 5, "Test will not work otherwise"
     await Timer(5, units="ns")
+    assert dut.count.value == 5, "5 ticks after reset"
 
-    dut._log.info("counter value: %s", dut.count.value)
-    assert True
+
+@cocotb.test()
+async def counter_resets_test(dut):
+    """Test that the counter properly resets"""
+    assert_defaults(dut)
+    await cocotb.start(Clock(dut.clk, 1, units="ns").start())
+    await reset(dut)
+
+    count = (STOP - START) // STEP // 2
+    await Timer(count, units="ns")
+    assert dut.count.value == count
+
+    dut.reset.value = 1
+    await Timer(5, units="ns")
+    assert dut.count.value == 0, "Counter is in reset"
+
+
+@cocotb.test()
+async def counter_rolls_over_test(dut):
+    """Test that the counter properly rolls over"""
+    assert_defaults(dut)
+    await cocotb.start(Clock(dut.clk, 1, units="ns").start())
+    await reset(dut)
+
+    count = (STOP - START) // STEP
+    await Timer(count, units="ns")
+    assert dut.count.value == count, "should be about to roll over"
+
+    await Timer(1, units="ns")
+    assert dut.count.value == START, "should have just rolled over"
+    assert dut.rollover.value == 1, "should have just rolled over"
+
+
+@cocotb.test()
+async def counter_enable_test(dut):
+    """Test that the counter properly handles enable"""
+    assert_defaults(dut)
+    await cocotb.start(Clock(dut.clk, 1, units="ns").start())
+    await reset(dut)
+
+    count = (STOP - START) // STEP // 2
+    await Timer(count, units="ns")
+    assert dut.count.value == count
+
+    dut.enable.value = 0
+    await Timer(10, units="ns")
+    assert dut.count.value == count, "counter not enabled"
+
+    dut.enable.value = 1
+    await Timer(1, units="ns")
+    assert dut.count.value == count + STEP, "should have increased once"
--- vaudeo/util/counter.v
+++ vaudeo/util/counter.v
@@ -1,31 +1,38 @@
 /******************************************************************************/
 /*                                                                            */
-/* Utility for a simple counter                                               */
+/* Utility for a simple up counter                                            */
 /*                                                                            */
 /******************************************************************************/
 
 `timescale 1ns/1ns
 
-module counter #(parameter  START = 0,
-                 parameter  STOP = 10,
-                 parameter  STEP = 1,
-                 localparam BITS = STEP > 0 ? $clog2(STOP - START)
-                                            : $clog2(START - STOP))
+module counter #(parameter  START = 0, // counter will start here
+                 parameter  STOP = 10, // counter will rollover when count is
+                                       // greater than this
+                 parameter  STEP = 1,  // how much count should increase each
+                                       // clock cycle
+                 localparam BITS = $clog2(STOP - START))
 
                 (input                  clk,       // clk for counter to run on
                  input                  reset,     // will reset count to START
                  input                  enable,    // will only count when enable high
                  output reg  [BITS-1:0] count,     // current value of counter
-                 output wire            rollover); // high if next clk will cause rollover
+                 output reg             rollover); // high if just rolled over
 
     wire [BITS-1:0] count_n;
+    wire            rollover_n;
 
-    assign rollover = count + STEP > STOP;
-    assign count_n = rollover ? START : count + STEP;
+    assign rollover_n = count + STEP > STOP;
+    assign count_n = rollover_n ? START : count + STEP;
 
-    always @(posedge clk) begin
-        if (reset) count <= START;
-        else if (enable) count <= count_n;
-    end
+    always @(posedge clk)
+        if (reset) begin
+            count <= START;
+            rollover <= 0;
+        end
+        else if (enable) begin
+            count <= count_n;
+            rollover <= rollover_n;
+        end
 
 endmodule // counter
